88b23f62ee36db520caae96a5bf043efbf6b5c24
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-08-16 12:13:53 -0700

    saving before closing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import GridBoard from './GridBoard';
 import Anime, { anime } from 'react-anime';
 
-let board = [
+let defaultBoard = [
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,27 +19,60 @@ let board = [
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
-const App = (props) => (
-  <div>
-    <Anime
-      delay={(e, i) => i * 1000}
-      scale={[.1, .9]}
-      >
-      <div>
-      hello
-      </div>
-      <div>
-      there
-      </div>
-      <div>
-      friend
-      </div>
-    </Anime>
-    <GridBoard
-      board={board}
-    />
-  </div>
-);
+
+const App : FC = () => {
+  const [board, setBoard] = useState(defaultBoard);
+  const [dimensions, setDimensions] = useState();
+
+  useEffect(() => {
+    console.log('page loaded')
+    makeBoard(40, 40, true);
+  }, [])
+
+  const makeBoard = (m, n, rand) => {
+    if(!m) {
+      m = 50;
+    }
+    if(!n) {
+      n = 50;
+    }
+
+    let matrix = [];
+    for(var i=0; i<m; i++) {
+        matrix[i] = [];
+        for(var j=0; j<n; j++) {
+            matrix[i][j] = rand? randomBinary() : 0;
+        }
+    }
+    setBoard(matrix);
+  }
+
+  const randomBinary = () => {
+    return Math.round(Math.random());
+  }
+
+  return (
+    <div>
+      <Anime
+        delay={(e, i) => i * 1000}
+        scale={[.1, .9]}
+        >
+        <div>
+        hello
+        </div>
+        <div>
+        there
+        </div>
+        <div>
+        friend
+        </div>
+      </Anime>
+      <GridBoard
+        board={board}
+      />
+    </div>
+  )
+  };
 
 export default App;
 
